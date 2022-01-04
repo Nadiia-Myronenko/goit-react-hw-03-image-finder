b476c7702fb78392a7b126560365170f441e8307
Nadiia-Myronenko <nadegda.burina@gmail.com>
2022-01-04 10:41:58 +0200

    installed nessesary libraries

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Wrapper from "./components/Wrapper/Wrapper.styled";
+
+class App extends Component {
+  render() {
+    return (
+      <Wrapper>
+
+      </Wrapper>
+    );
+  }
 }
 
 export default App;
